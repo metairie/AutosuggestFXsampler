1c618ab16a9f4277fec3bb491c9ee2548672501e
metairie <metairie@ebu.ch>
2015-09-04 10:52:08 +0200

    v 0.45.8 remove formatter

--- src/main/java/org/fxpart/ControllerWithItem.java
+++ src/main/java/org/fxpart/ControllerWithItem.java
@@ -33,7 +33,7 @@ public class ControllerWithItem implements Initializable {
         // don't change this
         autosuggest.setCacheDataMode(); // NOT ACCEPTING FREE VALUE
         autosuggest.setVisibleRowsCount(20);
-        autosuggest.setupFilter(o -> new MockDatas().loadLocation(), item -> String.format("%s . %s", item.getKey(), item.getValue()));
+        autosuggest.setupFilter(o -> new MockDatas().loadLocation(), item -> String.format("%s", item.getValue()));
         // works well
 
         // TODO BEGIN of temporary code - to be removed
